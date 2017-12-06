0dd755ab68d77603b3c558e54b4b51f5bd8cbd42
majid hamidian <majidehamidian@gmail.com>
2017-12-06 07:04:13 -0800

    added 3 h1 tag to Home.vue

--- ClientApp/components/Admin/Home/Home.vue
+++ ClientApp/components/Admin/Home/Home.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1 class="myh1">صفحه آغازین</h1>
+        <h1 class="myh1">صفحه آغازین</h1>
+        <h1 class="myh1">صفحه آغازین</h1>
+        <h1 class="myh1">صفحه آغازین</h1>
     </div>
 </template>
 
